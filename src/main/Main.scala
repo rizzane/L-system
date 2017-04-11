bbaeefe14e8f795451e530d73e39c6ccbbed3f0f
Joonas <Joonas@Joonas-PC.Home>
2017-04-11 21:25:18 +0300

    Fractal plant

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -5,27 +5,28 @@ import processing._
 
 class Main extends PApplet {
   
-//  variables: A, B
-//  axiom: A
-//  rules: (A -> AB), (B -> A)¨
-  var len = 100f
+  var length = 100f
   
-  var axiom = "F"
+  var axiom = "X"
   
   var sentence = axiom
   
-  var rule = {
+  val rule1 = {
     Map('F' -> "FF+[+F-F-F]-[-F+F+F]")
   }
   
+  val rule2 = Map('X' → "Y−[[X]+X]+Y[+YX]−X", 'Y' → "YY")
+  
   def generate() = {
-    len *= 0.5f
+    length *= 0.6f
     var nextSentence = ""
     for(i <- 0 until sentence.length) {
      var current = sentence.charAt(i)     
      nextSentence += {
        current match {
-       case 'F' => rule.get(current).get
+       case 'F' => rule1.get(current).get
+       case 'Y' => rule2.get(current).get
+       case 'X' => rule2.get(current).get
        case _ => current
        }
      }
@@ -35,14 +36,18 @@ class Main extends PApplet {
   }
   
   def grow() = {
-    translate(width/2, height)
+    translate(250, height)
     stroke(255)
     for(i <- 0 until sentence.length) {
       var current = sentence.charAt(i)
       current match {
         case 'F' => {
-          line(0,0,0,-len)
-          translate(0,-len)
+          line(0,0,0,-length)
+          translate(0,-length)
+        }
+        case 'Y' => {
+          line(0,0,0,-length)
+          translate(0,-length)          
         }
         case '+' => rotate(Math.PI.toFloat/7.2f)
         case '-' => rotate(-Math.PI.toFloat/7.2f)
@@ -54,23 +59,39 @@ class Main extends PApplet {
   }
   
   override def settings = {
-    size(600,600)
+    size(900,900)
   }
   
   
   override def setup = {
-    background(51)
+    background(0)
   }
   
   
   override def draw = {
     fill(255)
     rect(30,20,100,50)
+    rect(150, 20, 100, 50)
+    //Grow
+    textSize(20)
+    fill(0, 102, 153)
+    text("Grow", 40, 55)
+    //Change
+    text("Change", 160, 55)
   }
   
   override def mousePressed() = {
-    if(mouseX > 30 && mouseX < 130 && mouseY > 20 && mouseY < 70 && mousePressed) {      
+    if(mouseX > 30 && mouseX < 130 && mouseY > 20 && mouseY < 70) {      
       generate()
+    } else if(mouseX > 150 && mouseX < 250 && mouseY > 20 && mouseY < 70) {
+      background(0)
+      if(axiom == "X") {
+        axiom = "F"
+      } else {
+        axiom = "X"
+      }
+      sentence = axiom
+      length = 100
     }
   }
 }
